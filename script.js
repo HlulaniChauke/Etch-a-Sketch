1ed6f5ed31802f633443fc3536caf3b630ccb0dd
Hlulani Chauke <122245777+HlulaniChauke@users.noreply.github.com>
2023-11-08 22:04:30 +0900

    create prompt for user to enter # of boxes.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const gridContainer = document.querySelector('.drawingPadContainer');
+const resetButton = document.querySelector('#reset');
 //calculate the size for squares given the number.
 function squareSize(number){
     let size = 500 / number ;
@@ -8,24 +9,32 @@ function squareSize(number){
 //create a square
 function createSquare(size, idNum){
     const square = document.createElement('div');
-    square.style.border = '1px solid red';
+    square.style.border = '1px solid lightgrey';
     //square.textContent = 'wh'
     square.id = idNum;
     return square
 }
 
-let number = 50;
-let sizeOfSqr = squareSize(number);
+let number = 20;
 
+function drawGrid(number){
+    let sizeOfSqr = squareSize(number);
 
-for (let i = 1; i <= (number * number); i++){
+    for (let i = 1; i <= (number * number); i++){
     let newSqr = createSquare(sizeOfSqr, i)
     gridContainer.appendChild(newSqr);
-}
-//automate the generation of 1frs
-let frs = '1fr ';
-for (let i = 1; i < number; i++){
+    }
+    //automate the generation of 1frs
+    let frs = '1fr ';
+    for (let i = 1; i < number; i++){
     frs += '1fr ';
+    }   
+    gridContainer.style.gridTemplateColumns = frs ;
 }
-gridContainer.style.gridTemplateColumns = frs ;
 
+drawGrid(number);
+
+resetButton.addEventListener('click', () => 
+     drawGrid(Number(window.prompt("Type a number between 2 and 100", "16"))));
+
+    
